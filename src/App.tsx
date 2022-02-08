43dbc699126b42dfb20e025d4839f9b20badce90
Toms Mikoss <toms.mikoss@gmail.com>
2022-02-08 10:04:39 +0200

    Draw images

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react'
+
+import iconSrc from './images/good.png'
+import badIconSrc from './images/bad.png'
+import screenshotSrc from './images/screenshot.png'
+
+const timeout = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 function App() {
+  const fullCanvasRef = useRef<HTMLCanvasElement>(null)
+  const iconCanvasRef = useRef<HTMLCanvasElement>(null)
+  const imgRef = useRef<HTMLImageElement>(null)
+
+  const onLoad = async () => {
+    const fullCanvas = fullCanvasRef.current?.getContext('2d')
+    const iconCanvas = iconCanvasRef.current?.getContext('2d')
+    const image = imgRef.current
+
+    if (!fullCanvas || !iconCanvas || !image) {
+      return
+    }
+
+    const sourceW = 2560
+    const sourceH = 1440
+
+    const sourceX = sourceW * 0.06
+    const sourceY = sourceH * 0.3
+
+    const width = Math.ceil(sourceW * 0.2285)
+    const height = Math.ceil(sourceH * 0.407)
+
+    fullCanvas.canvas.width = width
+    fullCanvas.canvas.height = height
+
+    fullCanvas.drawImage(image, sourceX, sourceY, width, height, 0, 0, width, height)
+
+    const iconWidth = width / 8
+    const iconHeight = height / 8
+
+    const iconHeightWithoutTextRatio = 0.75
+
+    console.log({ width, height, iconWidth, iconHeight })
+
+    for (let iconX = 0; iconX < 8; iconX++) {
+      for (let iconY = 0; iconY < 8; iconY++) {
+        iconCanvas.drawImage(
+          image,
+          sourceX + iconX * iconWidth,
+          sourceY + iconY * iconHeight,
+          iconWidth,
+          iconHeight * iconHeightWithoutTextRatio,
+          0,
+          0,
+          iconWidth,
+          iconHeight * iconHeightWithoutTextRatio
+        )
+
+        await timeout(500)
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <canvas ref={iconCanvasRef} />
+      </div>
+
+      <div>
+        <canvas ref={fullCanvasRef} />
+      </div>
+
+      <img ref={imgRef} src={screenshotSrc} alt='' onLoad={onLoad} style={{ display: 'none' }} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
